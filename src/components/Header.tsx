7a0a4e61550b095bd354d2eddc8a51c2907d9293
Maaz Perwez <mzperwez786@gmail.com>
2025-06-29 20:10:17 +0530

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -117,7 +117,7 @@ function Navigation() {
 export default function Header() {
   return (
     <header className="bg-white shadow">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-28">
         {/* Mobile Left Section: Hamburger and Logo */}
         <div className="flex items-center md:hidden">
           <Navigation />
@@ -141,8 +141,8 @@ export default function Header() {
             <Image
               src="/logo.jpeg"
               alt="Viveon Global Healthcare Logo"
-              width={40}
-              height={40}
+              width={100}
+              height={100}
               className="mr-2"
               priority={true}
             />
